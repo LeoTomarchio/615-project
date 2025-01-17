d82908ed7ea368c1bf4331398d0d01ce8a37a874
LeoTomarchio <leandro.tomarchio@torontomu.ca>
2025-01-17 18:55:58 -0500

    Initial Values

---
+++ Variables.m
@@ -0,0 +1,42 @@
+%From Image
+Span = 35 %feet
+RootChord = 5+3/12 %feet
+TipChord = 3+6.2/12 %feet
+WingArea = 170 %feet^2
+ElevatorChord = 2+6/12 %feet
+ElevatorSpan = 12+11.75/12 %feet
+
+LandingGearLatDist = 10 %feet
+LandingGearLongDist = 5+8/12 %feet
+MinTurnRadius = 30 %feet
+FuseLength = 23+9.6/12 %feet
+RudderHeight = 7+3.6/12 %feet
+
+%Supplemental
+Engines = 1
+Horsepower = 160 %
+RPM = 2700 
+Bore = 5.125 %inches
+Stroke = 3.8175 %inches
+Displacement = 319.8 %inches^3
+CompRatio = 8.5 
+
+PropNumb = 1 
+BladeNum = 2 
+PropMaxDiam = 74 %inches
+PropMinDiam = 72 %inches
+
+FuelCap = 50 %gallons
+FuelUse = 48 %gallons
+
+MTOW = 2325 %pounds
+MLW = 2325 %pounds
+MaxCargo = 200 %pounds
+
+CargoVol = 24 %feet^3
+CargoWidth = 22 %inches
+CargoHeight = 20 %inches
+
+WingLoading = 13.7 %pounds per feet^2
+PowerLoading = 14.5 %pounds per horsepower
+
